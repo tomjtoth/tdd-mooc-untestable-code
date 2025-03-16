7764a418c5b46ec82216cb5258b808f14a63f574
Tamás Tóth <tomjtoth@gmail.com>
2025-03-16 12:51:38 +0200

    added test for the newly created fn readCSV

--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,14 +1,15 @@
-import { beforeEach, describe, test } from "vitest";
+import { describe, test } from "vitest";
 import { expect } from "chai";
-import { parsePeopleCsv } from "../src/untestable3.mjs";
+import { parsePeopleCsv, readCSV } from "../src/untestable3.mjs";
 import { writeFileSync } from "fs";
 
-// example input:
-// Loid,Forger,,Male
-// Anya,Forger,6,Female
-// Yor,Forger,27,Female
-
 describe("Untestable 3: CSV file parsing", () => {
+  test("the separated file reading function works", async () => {
+    const testString = "qwe,asd,yxc,åäö";
+    writeFileSync("test.csv", testString);
+    expect(await readCSV("test.csv")).to.equal(testString);
+  });
+
   test("parsePeopleCSV", () => {
     expect(
       parsePeopleCsv(`Loid,Forger,,Male
