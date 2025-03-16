03146425237f5e3cc9d442f521467558e5750626
Tamás Tóth <tomjtoth@gmail.com>
2025-03-16 12:19:37 +0200

    extracted die1 and die2 from fn diceHandValue

--- src/untestable2.mjs
+++ src/untestable2.mjs
@@ -4,9 +4,7 @@ function diceRoll() {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-export function diceHandValue() {
-  const die1 = diceRoll();
-  const die2 = diceRoll();
+export function diceHandValue(die1, die2) {
   if (die1 === die2) {
     // one pair
     return 100 + die1;
