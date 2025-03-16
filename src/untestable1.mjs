d9b3a40dfb722ebc1d7c36bdfc91bdd1c0339abf
Tamás Tóth <tomjtoth@gmail.com>
2025-03-16 12:04:44 +0200

    getting year from the passed in `now ` argument

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -4,7 +4,7 @@ export function daysUntilChristmas(now) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
-    christmasDay.setFullYear(new Date().getFullYear() + 1);
+    christmasDay.setFullYear(now.getFullYear() + 1);
   }
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
